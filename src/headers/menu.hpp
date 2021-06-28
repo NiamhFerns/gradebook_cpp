ef1d5417bf4e7186ab017ab2f090afc8ccc3dfef
KaosBunny <Niamh.K.Ferns@gmail.com>
2021-06-29 07:27:22 +1200

    Begun readding the mainmenu class.

--- src/headers/menu.hpp
+++ src/headers/menu.hpp
@@ -7,6 +7,19 @@
 #include "course.hpp"
 #include "misc.hpp"
 
+class MENU_Main {
+public:
+    void option1();
+    void option2();
+    void option3();
+    void option4();
+    void option5();
+    void help();
+    void quit();
+};
+
+//initialise menus
+
 //template <typename T>
 void callMenu();
 
--- src/menu.cpp
+++ src/menu.cpp
@@ -1,5 +1,33 @@
 #include "headers/menu.hpp"
 
+void MENU_Main::option1() {
+
+}
+
+void MENU_Main::option2() {
+
+}
+
+void MENU_Main::option3() {
+
+}
+
+void MENU_Main::option4() {
+
+}
+
+void MENU_Main::option5() {
+
+}
+
+void MENU_Main::help() {
+
+}
+
+void MENU_Main::quit() {
+
+}
+
 //TODO
 //Ideally each class would handle it's own help commands. This can be sorted out later.
 //Add help to callMenu function as menu.help(menu.help);
