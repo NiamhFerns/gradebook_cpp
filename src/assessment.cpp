47af8d5f12b29ebec1ab6bffdc7fe7ff22c901ce
KaosBunny <Niamh.K.Ferns@gmail.com>
2021-08-01 12:58:49 +1200

    Moved assesment stats into a single struct.
    If the assessment is not multipart, then the vector will stay one (this
    can be used as the multipart check).
    
    Assessment stats for the first assessment are just passed directly into
    the struct, then any more assessments can be added later via menu (this
    functionality will be added in move to ncurses...)
    
    c:

--- src/assessment.cpp
+++ src/assessment.cpp
@@ -1,30 +1,48 @@
 #include "headers/assessment.hpp"
 
-Assessment::Assessment
-(
-    std::string labelIn, 
-    std::string dueDateIn, 
-    std::string handinDateIn,
-    unsigned short gradeIn,
-    unsigned short maxGradeIn,
-    unsigned short weightingIn,
-    unsigned short latePenaltyIn
-)
+PartStats::PartStats (unsigned short partGradeIn,
+                      std::string labelIn,
+                      std::string dueDateIn,
+                      std::string handinDateIn,
+                      bool dropped) 
 {
-    
+
+};
+
+Assessment::Assessment(std::string labelIn, 
+                       std::string dueDateIn, 
+                       std::string handinDateIn,
+                       unsigned short gradeIn,
+                       unsigned short maxGradeIn,
+                       unsigned short weightingIn,
+                       unsigned short latePenaltyIn)
+{
+    //define the base assessment
+    //should there be only one assessment this will be the only part.
+    assessmentStats.push_back(
+        PartStats(
+            0,
+            "part-" + std::to_string(assessmentStats.size() + 1),
+            dueDateIn,
+            handinDateIn,
+            0 
+    ));
+
+
 }
 
 std::string Assessment::getLabel() {
+
 }
 
-unsigned short Assessment::getAssessedGrade() { return assessedGrade; }
+unsigned short Assessment::getAssessedGrade() { 
+    return 0; //magic owo
+}
 
 void Assessment::setAssessedGrade(unsigned short grade) {
         if (grade > maxGrade) {
             std::cout << "That is not a valid grade. Please try again.\n";
             return;
         }
-
-        assessedGrade = grade;
 }
 
--- src/headers/assessment.hpp
+++ src/headers/assessment.hpp
@@ -1,22 +1,35 @@
 #ifndef _ASSESSMENT_H
 #define _ASSESSMENT_H
 
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
 
-// struct PartStats { //struct of 
-//     unsigned short partGrade;
-//     std::string label, dueDate, handinDate;
-//     bool droppedFromScore;
-// };
+struct PartStats {
+    unsigned short partGrade;
+    std::string label, dueDate, handinDate;
+    bool droppedFromScore;
+
+    PartStats(unsigned short partGradeIn,
+              std::string labelIn,
+              std::string dueDateIn,
+              std::string handinDateIn,
+              bool dropped);
+};
 
 class Assessment{
 private:
-    //weighting is stored as a percentage but can be converted later.
-    unsigned short assessedGrade = 0, maxGrade = 100, weighting, latePenalty;
-    bool multiPart = 0;
-    //TO DO mark an assessmentStats tuple for the different parts. Default = 1.
+    //just use length of assessmentStats to determine whether multipart assessment. 
+    //if (length 1 ) x
+    //else y
+    std::vector<PartStats> assessmentStats;
+    std::string mainLabel;
 
+    //weighting is stored as a percentage but can be converted later.
+    //it is the total weighting for the whole assessment. 
+    //weighting per part handled later.
+    unsigned short maxGrade = 100, weighting, latePenalty;
+    
 public:
     Assessment
     (  
@@ -33,8 +46,8 @@ public:
     unsigned short getAssessedGrade(); 
 
     //takes in the amount of parts for multipart assessments.
-    void setMultiPart(unsigned short numParts);
     void setAssessedGrade(unsigned short grade);
+    void setAssessedGrade(unsigned short grade, unsigned short part);
 };
 
 #endif
